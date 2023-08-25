787d3995376acffb38c5d7a04419f52a43cd0c68
inwookim <dlsdn166@gmail.com>
2023-08-25 22:08:30 +0900

    feat : default basename 추가

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
 import "./styles.css";
+import {BrowserRouter as Router} from 'react-router-dom'; 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+    
   </React.StrictMode>,
   document.getElementById("root")
 );
